d691031e06a466adf38ff588844086146fc04e14
duongcoder211 <daomanhduong02112005@gmail.com>
2025-11-22 19:59:26 +0700

    Update test_stream_write.py

--- PythonLabs_year_1th/PyLab_7/test_stream_write.py
+++ PythonLabs_year_1th/PyLab_7/test_stream_write.py
@@ -11,15 +11,26 @@ class TestStreamWrite(unittest.TestCase):
     self.stream = io.StringIO()
 
     @logger(handle=self.stream)
-    def wrapped():
+    def success_wrapped():
+      return get_currencies(['USD'], url="https://www.cbr.ru/scripts/XML_daily.asp?date_req=09.10.2025")
+    
+    @logger(handle=self.stream)
+    def error_wrapped():
       return get_currencies(['USD'], url="https://invalid")
     
-    self.wrapped = wrapped
+    self.success_wrapped = success_wrapped
+    self.error_wrapped = error_wrapped
+
+  def test_logging_success(self):
+    self.success_wrapped()
+
+    logs = self.stream.getvalue()
+    self.assertIn("INFO", logs)
 
   def test_logging_error(self):
     # with self.assertRaises(ConnectionError):
     with self.assertRaises(requests.exceptions.RequestException):
-      self.wrapped()
+      self.error_wrapped()
 
     logs = self.stream.getvalue()
     # self.assertIn("ConnectionError", logs)
